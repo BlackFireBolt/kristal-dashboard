95883945d515f1e46b75eab35c04109ce43ec499
BlackFireBolt <boltward@gmail.com>
2021-07-08 17:11:34 +0300

    Uninstall garbage 2

--- src/config/vue-apex-charts-config.js
+++
@@ -1,7 +0,0 @@
-import Vue from "vue";
-import VueApexCharts from 'vue-apexcharts'
-import {Plotly} from "vue-plotly"
-Vue.use(VueApexCharts)
-Vue.use(Plotly)
-Vue.component('plotly', Plotly)
-Vue.component('apexchart', VueApexCharts)
--- src/main.js
+++ src/main.js
@@ -5,7 +5,6 @@ import vuetify from "./plugins/vuetify";
 import axios from "axios";
 
 import "./config/notification-config.js";
-import "./config/vue-apex-charts-config.js";
 import {store} from "./config/vuex-config.js";
 
 axios.defaults.headers.post['Content-Type'] = 'application/x-www-form-urlencoded';
